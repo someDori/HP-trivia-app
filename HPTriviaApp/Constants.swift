5265ce552e79a72fff610493cf43b2438cea5878
someDori <demetrepanjakidze1@gmail.com>
2024-12-11 17:59:42 +0400

    feat: add content to home screen

---
+++ HPTriviaApp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HPTriviaApp
+//
+//  Created by Demetre Panjakidze on 11.12.24.
+//
+
+import Foundation
+
+enum Constants {
+    static let hpFont = "PartyLetPlain"
+}
--- HPTriviaApp/ContentView.swift
+++ HPTriviaApp/ContentView.swift
@@ -9,13 +9,93 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            ZStack {
+                Image("hogwarts")
+                    .resizable()
+                    .frame(width: geo.size.width * 3, height: geo.size.height * 1.05)
+                
+                VStack {
+                    VStack {
+                        Image(systemName: "bolt.fill")
+                            .font(.largeTitle)
+                            .imageScale(.large)
+                        
+                        Text("HP")
+                            .font(.custom(Constants.hpFont, size: 70))
+                            .padding(.bottom, -50)
+                        
+                        Text("Trivia")
+                            .font(.custom(Constants.hpFont, size: 60))
+                    }
+                    .padding(.top, 120)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("Recent Scores")
+                            .font(.title2)
+                        
+                        Text("33")
+                        Text("27")
+                        Text("15")
+                    }
+                    .font(.title3)
+                    .padding(.horizontal)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.7))
+                    .clipShape(.rect(cornerRadius: 15))
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button {
+                            // show instructions screen
+                        } label: {
+                            Image(systemName: "info.circle.fill")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .shadow(radius: 5)
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            // start new game
+                        } label: {
+                            Text("Play")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .padding(.vertical, 7)
+                                .padding(.horizontal, 50)
+                                .background(.brown)
+                        }
+                        .clipShape(.rect(cornerRadius: 7))
+                        .shadow(radius: 5)
+                        
+                        Spacer()
+                        
+                        Button {
+                            // show settings screen
+                        } label: {
+                            Image(systemName: "gearshape.fill")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .shadow(radius: 5)
+                        }
+                        
+                        Spacer()
+                    }
+                    .frame(width: geo.size.width)
+                    
+                    Spacer()
+                }
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
