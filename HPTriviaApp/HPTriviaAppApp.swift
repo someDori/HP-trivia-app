37ec299f7f4d205f61a5936ac0b9ba3d4b845bfe
someDori <demetrepanjakidze1@gmail.com>
2024-12-11 17:13:10 +0400

    Initial commit

---
+++ HPTriviaApp/HPTriviaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaAppApp.swift
+//  HPTriviaApp
+//
+//  Created by Demetre Panjakidze on 11.12.24.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
