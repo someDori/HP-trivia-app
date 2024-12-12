7227f3a303b6156f22965f9787af8085fedb0977
someDori <demetrepanjakidze1@gmail.com>
2024-12-12 19:25:35 +0400

    feat: add functionality to End game button, activate hint buttons

--- HPTriviaApp/Screens/Gameplay.swift
+++ HPTriviaApp/Screens/Gameplay.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct Gameplay: View {
+    @Environment(\.dismiss) private var dismiss
     @State private var animateViewsIn = false
     @State private var tappedCorrectAnswer = false
     @State private var hintWiggle = false
     @State private var scaleNextButton = false
     @State private var movePointsToScore = false
+    @State private var revealHint = false
+    @State private var revealBook = false
     
     var body: some View {
         GeometryReader { geo in
@@ -25,7 +28,7 @@ struct Gameplay: View {
                     // MARK: Controls
                     HStack {
                         Button("End Game") {
-                            // TODO: End game
+                            dismiss()
                         }
                         .buttonStyle(.borderedProminent)
                         .tint(.red.opacity(0.5))
@@ -77,6 +80,26 @@ struct Gameplay: View {
                                             hintWiggle = true
                                         }
                                     }
+                                    .onTapGesture {
+                                        withAnimation(.easeOut(duration: 1)) {
+                                            revealHint = true
+                                        }
+                                    }
+                                    .rotation3DEffect(
+                                        .degrees(revealHint ? 1440 : 0),
+                                        axis: (x: 0, y: 1, z: 0)
+                                    )
+                                    .scaleEffect(revealHint ? 5 : 1)
+                                    .opacity(revealHint ? 0 : 1)
+                                    .offset(x: revealHint ? geo.size.width / 2 : 0)
+                                    .overlay(
+                                        Text("Hint")
+                                            .padding(.leading, 33)
+                                            .minimumScaleFactor(0.5)
+                                            .multilineTextAlignment(.center)
+                                            .opacity(revealHint ? 1 : 0)
+                                            .scaleEffect(revealHint ? 1.33 : 1)
+                                    )
                             }
                         }
                         .animation(
@@ -111,6 +134,26 @@ struct Gameplay: View {
                                             hintWiggle = true
                                         }
                                     }
+                                    .onTapGesture {
+                                        withAnimation(.easeOut(duration: 1)) {
+                                            revealBook = true
+                                        }
+                                    }
+                                    .rotation3DEffect(
+                                        .degrees(revealBook ? 1440 : 0),
+                                        axis: (x: 0, y: 1, z: 0)
+                                    )
+                                    .scaleEffect(revealBook ? 5 : 1)
+                                    .opacity(revealBook ? 0 : 1)
+                                    .offset(x: revealBook ? -geo.size.width / 2 : 0)
+                                    .overlay(
+                                        Image("hp1")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .padding(.trailing, 33)
+                                            .opacity(revealBook ? 1 : 0)
+                                            .scaleEffect(revealBook ? 1.23 : 1)
+                                    )
                             }
                         }
                         .animation(
@@ -241,8 +284,8 @@ struct Gameplay: View {
         }
         .ignoresSafeArea()
         .onAppear {
-//            animateViewsIn = true
-            tappedCorrectAnswer = true
+            animateViewsIn = true
+//            tappedCorrectAnswer = true
         }
     }
 }
