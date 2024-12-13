63058352422fb62367108c343d044e990655689a
someDori <demetrepanjakidze1@gmail.com>
2024-12-13 16:45:37 +0400

    feat: add functionality of clicking an answer and connect answers using matchedGeometryEffect

--- HPTriviaApp/Screens/Gameplay.swift
+++ HPTriviaApp/Screens/Gameplay.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct Gameplay: View {
     @Environment(\.dismiss) private var dismiss
+    @Namespace private var namespace
     @State private var animateViewsIn = false
     @State private var tappedCorrectAnswer = false
     @State private var hintWiggle = false
@@ -16,6 +17,8 @@ struct Gameplay: View {
     @State private var movePointsToScore = false
     @State private var revealHint = false
     @State private var revealBook = false
+    
+    let tempAnswers = [true, false, false, false]
     
     var body: some View {
         GeometryReader { geo in
@@ -73,9 +76,9 @@ struct Gameplay: View {
                                     .onAppear {
                                         withAnimation(
                                             .easeInOut(duration: 0.1)
-                                            .repeatCount(9)
-                                            .delay(5)
-                                            .repeatForever()
+                                                .repeatCount(9)
+                                                .delay(5)
+                                                .repeatForever()
                                         ) {
                                             hintWiggle = true
                                         }
@@ -127,9 +130,9 @@ struct Gameplay: View {
                                     .onAppear {
                                         withAnimation(
                                             .easeInOut(duration: 0.1)
-                                            .repeatCount(9)
-                                            .delay(5)
-                                            .repeatForever()
+                                                .repeatCount(9)
+                                                .delay(5)
+                                                .repeatForever()
                                         ) {
                                             hintWiggle = true
                                         }
@@ -167,26 +170,72 @@ struct Gameplay: View {
                     // MARK: Answers
                     LazyVGrid(columns: [GridItem(), GridItem()]) {
                         ForEach(1 ..< 5) { i in
-                            VStack {
-                                if animateViewsIn {
-                                    Text("Answer \(i)")
-                                        .minimumScaleFactor(0.5)
-                                        .multilineTextAlignment(.center)
-                                        .padding(10)
-                                        .frame(
-                                            width: geo.size.width / 2.15,
-                                            height: 80
-                                        )
-                                        .background(.green.opacity(0.5))
-                                        .clipShape(.rect(cornerRadius: 25))
-                                        .transition(.scale)
+                            if tempAnswers[i - 1] {
+                                VStack {
+                                    if animateViewsIn {
+                                        if !tappedCorrectAnswer {
+                                            Text("Answer \(i)")
+                                                .minimumScaleFactor(0.5)
+                                                .multilineTextAlignment(.center)
+                                                .padding(10)
+                                                .frame(
+                                                    width: geo.size.width / 2.15,
+                                                    height: 80
+                                                )
+                                                .background(.green.opacity(0.5))
+                                                .clipShape(.rect(cornerRadius: 25))
+                                                .transition(
+                                                    .asymmetric(
+                                                        insertion: .scale,
+                                                        removal:
+                                                        .scale(scale: 5)
+                                                            .combined(
+                                                                with: .opacity
+                                                                    .animation(
+                                                                        .easeOut(duration: 0.5)
+                                                                    )
+                                                            )
+                                                    )
+                                                )
+                                                .matchedGeometryEffect(
+                                                    id: "answer",
+                                                    in: namespace
+                                                )
+                                                .onTapGesture {
+                                                    withAnimation(.easeOut(duration: 1)) {
+                                                        tappedCorrectAnswer = true
+                                                    }
+                                                }
+                                        }
+                                    }
                                 }
+                                .animation(
+                                    .easeOut(duration: 1)
+                                        .delay(1.5),
+                                    value: animateViewsIn
+                                )
+                            } else {
+                                VStack {
+                                    if animateViewsIn {
+                                        Text("Answer \(i)")
+                                            .minimumScaleFactor(0.5)
+                                            .multilineTextAlignment(.center)
+                                            .padding(10)
+                                            .frame(
+                                                width: geo.size.width / 2.15,
+                                                height: 80
+                                            )
+                                            .background(.green.opacity(0.5))
+                                            .clipShape(.rect(cornerRadius: 25))
+                                            .transition(.scale)
+                                    }
+                                }
+                                .animation(
+                                    .easeOut(duration: 1)
+                                        .delay(1.5),
+                                    value: animateViewsIn
+                                )
                             }
-                            .animation(
-                                .easeOut(duration: 1)
-                                    .delay(1.5),
-                                value: animateViewsIn
-                            )
                         }
                     }
                     
@@ -250,6 +299,10 @@ struct Gameplay: View {
                             .background(.green.opacity(0.5))
                             .clipShape(.rect(cornerRadius: 25))
                             .scaleEffect(2)
+                            .matchedGeometryEffect(
+                                id: "answer",
+                                in: namespace
+                            )
                     }
                     
                     Spacer()
